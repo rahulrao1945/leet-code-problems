c9c4b22388dba800c2b683548805bd3066cd8c5d
rahulrao1945 <raorahul11123@gmail.com>
2026-07-04 18:22:44 +0530

    Time: 6 ms (12.29%) | Memory: 9 MB (25.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+
+    int temp = x;
+    long rev = 0;
+
+    while (x > 0) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+
+    return rev == temp;
+}
