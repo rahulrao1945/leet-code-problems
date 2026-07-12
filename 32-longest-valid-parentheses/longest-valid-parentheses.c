d918d190505757e59b0b312c3a6948980ac98cb5
Rahul Yadav <raorahul11123@gmail.com>
2026-07-12 21:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (19.12%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdlib.h>
+
+int longestValidParentheses(char* s) {
+    int n = strlen(s);
+    int *stack = (int *)malloc((n + 1) * sizeof(int));
+
+    int top = 0;
+    stack[top] = -1;
+
+    int maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(') {
+            stack[++top] = i;
+        } else {
+            top--;
+
+            if (top < 0) {
+                stack[++top] = i;
+            } else {
+                int len = i - stack[top];
+                if (len > maxLen)
+                    maxLen = len;
+            }
+        }
+    }
+
+    free(stack);
+    return maxLen;
+}
