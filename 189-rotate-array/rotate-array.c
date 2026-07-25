872601c4e1281052acb07f00fd64c6151cee0796
Rahul Yadav <raorahul11123@gmail.com>
2026-07-25 22:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (5.45%) - LeetSync

---
+++ 189-rotate-array/rotate-array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+void rotate(int* nums, int numsSize, int k)
+{
+    k = k % numsSize;
+
+    int *temp = (int *)malloc(numsSize * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        temp[(i + k) % numsSize] = nums[i];
+    }
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        nums[i] = temp[i];
+    }
+
+    free(temp);
+}
